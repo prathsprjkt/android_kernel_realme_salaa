d24db5bac423c49f597e1e1048a461593825db11
prathamdby <134331217+prathamdby@users.noreply.github.com>
2024-01-28 22:57:28 +0530

    script: Import the build script for the Bolt kernel
    Signed-off-by: prathamdby <134331217+prathamdby@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -1,48 +1,81 @@
-#!/bin/bash
-function compile()
-{
-source ~/.bashrc && source ~/.profile
-export LC_ALL=C && export USE_CCACHE=1
-ccache -M 25G
-TANGGAL=$(date +"%Y%m%d-%H")
-export ARCH=arm64
-export KBUILD_BUILD_HOST=android-build
-export KBUILD_BUILD_USER="kardebayan"
-clangbin=clang/bin/clang
-if ! [ -a $clangbin ]; then git clone --depth=1 https://github.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-6443078 clang
-fi
-gcc64bin=gcc64/bin/aarch64-linux-android-as
-if ! [ -a $gcc64bin ]; then git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 gcc64
-fi
-gcc32bin=gcc32/bin/arm-linux-androideabi-as
-if ! [ -a $gcc32bin ]; then git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 gcc32
-fi
-rm -rf AnyKernel
-make O=out ARCH=arm64 salaa_defconfig
-PATH="${PWD}/clang/bin:${PATH}:${PWD}/gcc32/bin:${PATH}:${PWD}/gcc64/bin:${PATH}" \
-make -j$(nproc --all) O=out \
-                      ARCH=arm64 \
-                      CC="clang" \
-                      CLANG_TRIPLE=aarch64-linux-gnu- \
-                      CROSS_COMPILE="${PWD}/gcc64/bin/aarch64-linux-android-" \
-                      CROSS_COMPILE_ARM32="${PWD}/gcc32/bin/arm-linux-androideabi-" \
-                      LD=ld.lld \
-                      CONFIG_NO_ERROR_ON_MISMATCH=y
+#!/bin/env bash
+
+# Define compile function
+function compile() {
+  # Load environment variables
+  source ~/.bashrc
+  source ~/.profile
+
+  # Set environment variables
+  export LC_ALL=C
+  export USE_CCACHE=1
+  export ARCH=arm64
+  export SUBARCH=arm64
+
+  export KBUILD_BUILD_HOST="PrathamsBuilds"
+  export KBUILD_BUILD_USER="Pratham"
+
+  # Allocate 100GB of memory to ccache
+  ccache -M 100G
+
+  # Download clang if not present
+  if [[ ! -d "clang" ]]; then
+    aria2c -x16 -s16 https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r498229b.tar.gz
+    mkdir clang
+    tar -xf clang-r498229b.tar.gz -C clang
+    rm -rf clang-r498229b.tar.gz
+  fi
+
+  # Clean and create output directory
+  if [[ -d "out" ]]; then
+    rm -rf out/
+    mkdir -p "out"
+  fi
+
+  # Build the kernel
+  make -j$(nproc --all) O=out ARCH=${ARCH} SUBARCH=${SUBARCH} salaa_defconfig
+
+  # Add clang bin directory to PATH
+  PATH="${PWD}/clang/bin:${PATH}"
+
+  # Build the kernel with clang and log output
+  make -j$(nproc --all) O=out CC="clang" LLVM=1 CONFIG_NO_ERROR_ON_MISMATCH=y 2>&1 | tee build.log
 }
-function zupload()
-{
-zimage=out/arch/arm64/boot/Image.gz
-if ! [ -a $zimage ];
-then
-echo  " Failed To Compile Kernel"
-else
-echo -e " Kernel Compile Successful"
-git clone --depth=1 https://github.com/kardebayan/AnyKernel3.git AnyKernel
-cp out/arch/arm64/boot/Image.gz AnyKernel
-cd AnyKernel
-zip -r9 Stormbreaker-salaa-${TANGGAL}.zip *
-cd ../
-fi
+
+# Define upload file function
+function upload_file() {
+  local filepath="$1"
+  link=$(curl --upload-file "$filepath" https://free.keep.sh/)
+  echo "$link"
 }
+
+# Define upload function
+function upload() {
+  # Check if kernel image exists
+  if [[ ! -f "out/arch/${ARCH}/boot/Image.gz-dtb" ]]; then
+    echo "ERROR: No kernel image found! Exiting now..."
+    upload_file "build.log"
+    exit 1
+  fi
+
+  # Clean and clone AnyKernel repository
+  if [[ -d "AnyKernel" ]]; then
+    rm -rf AnyKernel/
+  fi
+  git clone --single-branch --depth 1 -b salaa https://github.com/prathamdby/AnyKernel3.git AnyKernel
+
+  # Copy image to AnyKernel and zip the package
+  cp "out/arch/${ARCH}/boot/Image.gz-dtb" AnyKernel
+  cd AnyKernel/
+  local date=$(date +'%d%m%y')
+  local file="Bolt-ZAP!-Kernel-V1-salaa-$date-release.zip"
+  zip -r9 "$file" *
+
+  # Upload the AnyKernel package
+  upload_file "$file"
+}
+
+# Run functions
 compile
-zupload
+upload
+
